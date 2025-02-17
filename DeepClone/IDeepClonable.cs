ac1a845fe9a226f5756261bf7f30f63cd01e80c5
Matt Edmondson <matthew.edmondson@gmail.com>
2025-02-17 15:23:49 +1100

    Apply new editorconfig

--- DeepClone/IDeepClonable.cs
+++ DeepClone/IDeepClonable.cs
@@ -12,5 +12,5 @@ public interface IDeepCloneable<out T> where T : class
 	/// Creates a new <typeparamref name="T" /> that is a deep copy of the current instance.
 	/// </summary>
 	/// <returns>The <typeparamref name="T" />.</returns>
-	T DeepClone();
+	public T DeepClone();
 }
