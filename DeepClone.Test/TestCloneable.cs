4f1ce75d432418ed5b6df6bf8d9928516a932ba3
Matt Edmondson <matthew.edmondson@gmail.com>
2025-05-04 20:00:36 +1000

    Remove Directory.Build.props and Directory.Build.targets files, delete unused scripts for managing metadata, changelog, license, and versioning. Add copyright headers to IDeepClonable.cs and TestCloneable.cs.

--- DeepClone.Test/TestCloneable.cs
+++ DeepClone.Test/TestCloneable.cs
@@ -1,3 +1,7 @@
+// Copyright (c) ktsu.dev
+// All rights reserved.
+// Licensed under the MIT license.
+
 namespace ktsu.DeepClone.Tests;
 
 public class TestCloneable(int value) : IDeepCloneable<TestCloneable>
--- DeepClone/IDeepClonable.cs
+++ DeepClone/IDeepClonable.cs
@@ -1,3 +1,7 @@
+// Copyright (c) ktsu.dev
+// All rights reserved.
+// Licensed under the MIT license.
+
 namespace ktsu.DeepClone;
 
 // https://github.com/ppy/osu/blob/master/osu.Game/Utils/IDeepCloneable.cs
